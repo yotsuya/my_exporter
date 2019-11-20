f873d3a6c71b0115cbf69e71aef824a3f60bcd90
Kenzo Yotsuya <1560182+yotsuya@users.noreply.github.com>
2019-11-20 17:08:48 +0900

    Update entrypoint.sh

--- .github/actions/openio/entrypoint.sh
+++ .github/actions/openio/entrypoint.sh
@@ -7,7 +7,12 @@ PID=$!
 # TODO: openioの起動完了をチェックする方法調べる
 sleep 60
 
+date
 /my_exporter
 
+# OpenIOコンテナイメージのバージョンにもよるが、単にopenio-docker-init.shをkillするだけでは
+# gracefulに停止しない場合がある。ここではコンテナは使い捨てなので実用上は問題ないのだが、
+# 画面がエラー出力であふれるのが嫌なので、先にgridinit_cmd stopを実行しておく。
+gridinit_cmd stop
 kill $PID
 wait $PID
