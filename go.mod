86edaf121ce50efffd5a6bfbf2b86fdeeed0a5c4
Kenzo Yotsuya <1560182+yotsuya@users.noreply.github.com>
2019-11-25 19:48:40 +0900

    Add example exporter

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/yotsuya/my_exporter
+
+go 1.13
+
+require (
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/go-kit/kit v0.9.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/prometheus/client_golang v1.2.1
+	github.com/prometheus/common v0.7.0
+	github.com/prometheus/procfs v0.0.7 // indirect
+	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+)
--- my_exporter.go
+++ my_exporter.go
@@ -1,9 +1,99 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
+	"sync"
+
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
+const (
+	namespace = "openio"
+)
+
+var (
+	up = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "", "up"),
+		"Was the last scrape of OpenIO successful.",
+		nil, nil,
+	)
+)
+
+type Exporter struct {
+	mutex sync.RWMutex
+	// up     prometheus.Gauge
+	logger log.Logger
+}
+
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	ch <- up
+}
+
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+
+	ch <- prometheus.MustNewConstMetric(up, prometheus.GaugeValue, 1)
+}
+
+func NewExporter(logger log.Logger) (*Exporter, error) {
+	return &Exporter{
+		logger: logger,
+	}, nil
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	var (
+		listenAddress = kingpin.Flag(
+			"web.listen-address",
+			"Address to listen on for web interface and telemetry.",
+		).Default(":9109").String()
+		metricsPath = kingpin.Flag(
+			"web.telemetry-path",
+			"Path under which to expose metrics.",
+		).Default("/metrics").String()
+	)
+
+	promlogConfig := &promlog.Config{}
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	kingpin.Version(version.Print("openio_exporter"))
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+	logger := promlog.New(promlogConfig)
+
+	level.Info(logger).Log("msg", "Starting openio_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Build context", "context", version.BuildContext())
+
+	exporter, err := NewExporter(logger)
+	if err != nil {
+		level.Error(logger).Log("msg", "Error creating an exporter", "err", err)
+		os.Exit(1)
+	}
+	prometheus.MustRegister(exporter)
+	prometheus.MustRegister(version.NewCollector("openio_exporter"))
+
+	http.Handle(*metricsPath, promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+<head><title>Haproxy Exporter</title></head>
+<body>
+<h1>OpenIO Exporter</h1>
+<p><a href='` + *metricsPath + `'>Metrics</a></p>
+</body>
+</html>`))
+	})
+
+	level.Info(logger).Log("msg", "Listening on address", "address", *listenAddress)
+	if err := http.ListenAndServe(*listenAddress, nil); err != nil {
+		level.Error(logger).Log("msg", "Error starting HTTP server", "err", err)
+		os.Exit(1)
+	}
 }
